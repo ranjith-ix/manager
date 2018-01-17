c2e5eb911a0fecc8653e371f142c16a404b764ad
ranjith <ranjithkumar.s@ionixxtech.com>
2018-01-17 15:34:55 +0530

    nav-setup-17/01/18

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import {Scene,Router} from 'react-native-router-flux';
+import { Scene,Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 
 const RouterComponent=()=>{
     return(
         <Router>
-            <Scene key="login" component={LoginForm} title="Please Login"/>
+        <Scene>
+            <Scene key="login" component={LoginForm} title="Please Login" />
+        </Scene>
         </Router>
     );
 };
